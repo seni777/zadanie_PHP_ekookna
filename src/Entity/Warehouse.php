a7f69e0f8fce60bdc5431624ee6bb7b03b937f98
seni777 <basti12@o2.pl>
2023-03-22 21:14:30 +0100

    dodanie dashboardu

---
+++ src/Entity/Warehouse.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WarehouseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WarehouseRepository::class)]
+class Warehouse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street = null;
+
+    #[ORM\Column]
+    private ?int $warehouseNumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getWarehouseNumber(): ?int
+    {
+        return $this->warehouseNumber;
+    }
+
+    public function setWarehouseNumber(int $warehouseNumber): self
+    {
+        $this->warehouseNumber = $warehouseNumber;
+
+        return $this;
+    }
+}
